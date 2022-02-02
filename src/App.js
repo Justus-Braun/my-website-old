8394b4110564a859afc7c1bf0f933b0d4d9f037a
Justus Braun <justusbraun03@gmail.com>
2022-02-03 00:29:09 +0100

    Added Mobile compatibility

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Button from './components/Button';
 import GithubLogo from './assets/github.svg';
@@ -6,15 +7,41 @@ import CodeLogo from './assets/code.svg';
 import MailLogo from './assets/mail.svg';
 
 function App() {
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+  React.useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+
+    }
+    window.addEventListener('resize', handleResize)
+    return _ => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+  var svgMultiplyer = 20;
+  
+  const buttons = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: dimensions.width / svgMultiplyer < 50 ? "column" : "row"
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Justus Braun</h1>
-        <div className="buttons">
-          <Button imgSrc={CodeLogo} onButtonClick={() => window.location.href = "https://github.com/Justus-Braun/my-website"} />
-          <Button imgSrc={GithubLogo} onButtonClick={() => window.location.href = "https://github.com/Justus-Braun"} />
-          <Button imgSrc={DiscordLogo} onButtonClick={() => window.location.href = "https://gflp10.xyz/online"} />
-          <Button imgSrc={MailLogo} onButtonClick={() => window.location.href = "mailto:justusbraun03@gmail.com"} />
+        <div style={buttons}>
+          <Button imgSrc={CodeLogo} onButtonClick={() => window.location.href = "https://github.com/Justus-Braun/my-website"} svgMultiplyer={svgMultiplyer} dimensions={dimensions} />
+          <Button imgSrc={GithubLogo} onButtonClick={() => window.location.href = "https://github.com/Justus-Braun"} svgMultiplyer={svgMultiplyer} dimensions={dimensions} />
+          <Button imgSrc={DiscordLogo} onButtonClick={() => window.location.href = "https://gflp10.xyz/online"} svgMultiplyer={svgMultiplyer} dimensions={dimensions} />
+          <Button imgSrc={MailLogo} onButtonClick={() => window.location.href = "mailto:justusbraun03@gmail.com"} svgMultiplyer={svgMultiplyer} dimensions={dimensions} />
         </div>
       </header>
     </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './button.css';
 
-export default function Button({ imgSrc, style, onButtonClick }) {
+export default function Button({ imgSrc, style, onButtonClick, svgMultiplyer, dimensions }) {
+
+    const imgSize = dimensions.width / svgMultiplyer < 50 ? dimensions.width / svgMultiplyer + 50 : dimensions.width / svgMultiplyer;
     return (
         <button
             className="button"
             style={style}
             onClick={onButtonClick}
         >
-            <img src={imgSrc} width="100vw" height="100vw" alt="submit"/>
+            <img src={imgSrc} width={imgSize} height={imgSize} alt="submit" />
         </button>
     );
 }
