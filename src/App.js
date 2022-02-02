9e2c695f369d905498e58babfe2558d2ab75c0fc
Justus Braun <justusbraun03@gmail.com>
2022-02-03 00:07:16 +0100

    Buttons redesign and working now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import GithubLogo from './assets/github.svg';
+import DiscordLogo from './assets/discord.svg';
+import CodeLogo from './assets/code.svg';
+import MailLogo from './assets/mail.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>  
-        <Button label="hallo" backgroundColor="green"/>
+        <h1>Justus Braun</h1>
+        <div className="buttons">
+          <Button imgSrc={CodeLogo} onButtonClick={() => window.location.href = "https://github.com/Justus-Braun/my-website"} />
+          <Button imgSrc={GithubLogo} onButtonClick={() => window.location.href = "https://github.com/Justus-Braun"} />
+          <Button imgSrc={DiscordLogo} onButtonClick={() => window.location.href = "https://gflp10.xyz/online"} />
+          <Button imgSrc={MailLogo} onButtonClick={() => window.location.href = "mailto:justusbraun03@gmail.com"} />
+        </div>
       </header>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,14 @@
 import React from "react";
 import './button.css';
 
-class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {color: "red"};
-    }
-    
-    render() {
-        return <p className="button" style={{
-            backgroundColor: this.props.backgroundColor
-        }}>{this.props.label}</p>;
-    }
+export default function Button({ imgSrc, style, onButtonClick }) {
+    return (
+        <button
+            className="button"
+            style={style}
+            onClick={onButtonClick}
+        >
+            <img src={imgSrc} width="100vw" height="100vw" alt="submit"/>
+        </button>
+    );
 }
-
-export default Button;
